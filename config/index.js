ccea5572ab52660bdbacca19f2cbff1c19a7256e
xiehui999 <xiehui749835085>
2021-07-14 15:09:51 +0800

    classname bug

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  projectName: 'test',
-  date: '2020-8-30',
+  projectName: 'taroDemo3.2.10',
+  date: '2021-6-5',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
@@ -41,6 +41,17 @@ const config = {
         }
       }
     }
+  },
+  rn:{
+    postcss: {
+      cssModules: {
+        enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
   },
   h5: {
     publicPath: '/',
--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,6 @@
 export default {
   pages: [
-    'pages/index/index',
-    'pages/index/TestPage'
+    'pages/index/index'
   ],
   window: {
     backgroundTextStyle: 'light',
